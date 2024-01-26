341b7cdf395321799dc2576df245db0c209081f5
shilpa053020 <shilpa053020@gmail.com>
2024-01-26 13:52:23 +0530

    Id:501 Issue:Update Course. Fixed:Added functionlity for update course. Testing:Tested using Insomina.:

--- server/src/Routes/adminRoute.js
+++ server/src/Routes/adminRoute.js
@@ -1,11 +1,12 @@
 import exp from "express"
-import signupAdmin, { addCourse, loginAdmin } from "../Controller/adminController.js";
+import signupAdmin, { addCourse, loginAdmin, updateCourse } from "../Controller/adminController.js";
 import { verifyToken } from "../Middleware/auth.js";
 
 const router = exp.Router()
 
 router.post("/signup", signupAdmin);
 router.post("/login", loginAdmin)
-router.post("/addcourse",verifyToken, addCourse)
+router.post("/addcourse", verifyToken, addCourse)
+router.put("/updatecourse/:id", verifyToken, updateCourse)
 
 export default router;
