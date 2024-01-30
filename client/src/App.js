7c7fc2c9913971acef0e756f00b817c835962d47
Bala Shanmugam <compsciuniv@gmail.com>
2024-01-30 15:47:23 +0530

    Add Landing component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Appbar } from "./components/Appbar";
+import { Landing } from './components/Landing';
 
 function App() {
   return (
     <div>
       <Router>
         <Appbar />
+        <Routes>
+          <Route path={"/"} element={<Landing />} />
+        </Routes>
       </Router>
     </div>
   );
---
+++ client/src/components/Landing.jsx
@@ -0,0 +1,21 @@
+import { Grid, Typography } from "@mui/material";
+import { Signinup } from "./Appbar";
+
+export const Landing = () => {
+  return (
+    <div>
+      <Grid container style={{ padding: "5vw" }}>
+        <Grid item xs={12} md={6} lg={6}>
+          <div style={{ marginTop: 100 }}>
+            <Typography variant={"h2"}>Freshkite</Typography>
+            <Typography variant={"h5"}>Earn When U Learn</Typography>
+            <Signinup />
+          </div>
+        </Grid>
+        <Grid item xs={12} md={6} lg={6} style={{ marginTop: 20 }}>
+          <img src={"/CourseDetails.png"} alt="File not found" width={"100%"} />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
