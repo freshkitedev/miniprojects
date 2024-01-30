aea644c8d2ead9bb723ec9fb0b90c79232363c6f
Bala Shanmugam <compsciuniv@gmail.com>
2024-01-30 15:47:23 +0530

    Add Landing component

--- client/src/components/Appbar.jsx
+++ client/src/components/Appbar.jsx
@@ -1,4 +1,4 @@
-import { Button, Typography } from "@mui/material";
+import { Button} from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import "./Appbar.css";
 const Coursera = () => {
@@ -10,30 +10,30 @@ const Coursera = () => {
         navigate("/");
       }}
     >
-      <Typography variant="h5">Coursera</Typography>
+      <img src="/freshkite_logo.jpg" alt="Logo not found" width={150} />
     </div>
   );
 };
 
-const Signinup = () => {
-    return (
-        <div className="sbtn">
+export const Signinup = () => {
+  return (
+    <div className="sbtn">
+      <div>
         <Button style={{ backgroundColor: "#4CAF50" }} variant="contained">
           Signup
         </Button>
-        <div className="signin">
-          <Button variant="contained">Signin</Button>
-        </div>
       </div>
-
-    )
-}
+      <div className="signin">
+        <Button variant="contained">Signin</Button>
+      </div>
+    </div>
+  );
+};
 export const Appbar = () => {
   return (
     <div className="appbar">
       <Coursera />
       <Signinup />
-      
     </div>
   );
 };
